2a9e1e4e61cd8c29342442fb881f2b172749ab65
Jérémy Marodon <marodon.jeremy@gmail.com>
2015-09-30 23:40:10 +0200

    [MOD] Configuration root node

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('a_designs_calendar');
+        $treeBuilder->root('th3mouk_full_calendar');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
